fda51e217221aff69230abd56f516afb5ea9ac8e
IcicleF <icicle.flw@gmail.com>
2018-04-07 19:51:00 +0800

    fixed smalllittlebug

--- src/logic.cpp
+++ src/logic.cpp
@@ -377,10 +377,7 @@ void GameLogic::getRank(int *rank)
                 pptrs[j] = tmp;
             }
     for (int i = 0; i < playerCount; ++i)
-	{
-		cout << pptrs[i]->score << " " << pptrs[i]->gold << endl;
         rank[i] = pptrs[i]->id;
-	}
 }
 
 //新回合刷新
